277b8f16b0b79249357851b2d669977c6f6e2fce
Nikita Mitasov <32384814+ch4og@users.noreply.github.com>
2023-02-17 02:40:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ def handle_message(message):
         else:
             bot.reply_to(message, f"Подписавшись на уведомления вы будете получать сообщения каждый раз когда {os.getenv('STREAMER')} запускает стрим. \nВ случае ошибок пишите @{os.getenv(DEV)} \n\nВы не подписаны на уведомления.", reply_markup=keyboard)
     elif (message.text == "sysi"):
-        if (message.chat.username == "ch4og"):
+        if (message.chat.username == os.getenv(DEV)):
             with open('msg.log', 'r') as file:
                 logg = file.read()
             bot.reply_to(message, f"online={online}\n\nLOG:\n{logg}", reply_markup=keyboard)
