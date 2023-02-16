5606241ef19d30b059d874da81948f588b448ff7
ch4og <mitanick@ya.ru>
2023-02-16 19:06:51 +0300

    Bot done

--- bot.py
+++ bot.py
@@ -1,61 +1,112 @@
-import telegram
+import telebot
+import time
 import os
+import threading
+import json
 import requests
+
 from dotenv import load_dotenv
-from telegram.ext import CommandHandler, Updater
 
 load_dotenv()
 
-
-# Set up Twitch API client and get streamer ID
-headers = {
-    'Client-ID': os.getenv('TW_CLIENT'),
-    'Authorization': f'Bearer {os.getenv("TW_OAUTH")}'
-}
-params = {
-    'login': os.getenv('idobibot')
-}
-response = requests.get('https://api.twitch.tv/helix/users', headers=headers, params=params).json()
-streamer_id = response['data'][0]['id']
-
-# Set up Telegram bot
-bot = telegram.Bot(token=os.getenv('TG_API'))
-updater = Updater(token=os.getenv('TG_API'), use_context=True)
+bot = telebot.TeleBot(os.getenv('TG_API'))
 
 # Dictionary to store subscribed chat IDs
-subscribers = {}
 
-# Handler for /start command
-def start(update, context):
-    # Add the user's chat ID to the subscribers dictionary
-    chat_id = update.message.chat_id
-    subscribers[chat_id] = True
-    update.message.reply_text('Вы подписались на уведомления. Используйте /stop для отписки.')
+online = 0
 
-# Handler for /stop command
-def stop(update, context):
-    # Remove the user's chat ID from the subscribers dictionary
-    chat_id = update.message.chat_id
+keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.add(*[telebot.types.KeyboardButton(butt) for butt in ['Подписаться на уведомления', 'Отписаться от уведомлений']])
+
+@bot.message_handler(commands = ['start'])
+def start(message):
+    bot.reply_to(message, f'Привет!\nЭто бот для уведомлений о стримах на канале {os.getenv("STREAMER")}!', reply_markup=keyboard)
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if (message.text == "Подписаться на уведомления"):
+        if add_to_subs(message.chat.id):
+            bot.reply_to(message, f'Вы успешно подписались.')
+            log(f"{message.chat.username} subscribed ({message.chat.id})")
+        else:
+            bot.reply_to(message, f'Вы уже подписаны.')
+        
+    elif (message.text == "Отписаться от уведомлений"):
+        if rem_from_subs(message.chat.id):
+            bot.reply_to(message, f'Вы успешно отписались.')
+            log(f"{message.chat.username} unsubscribed ({message.chat.id})")
+        else:
+            bot.reply_to(message, f'Вы не были подписаны.')
+        
+    else:
+         bot.reply_to(message, f'Извините, я вас не понял, используйте кнопки. В случае ошибки воспользуйтесь /start')
+         log(f"!!!{message.chat.username} - {message.text} ({message.chat.id})")
+         
+def check_stream_status():
+    global subscribers
+    headers = {
+        'Client-ID': os.getenv('TW_CLIENT'),
+        'Authorization': f"Bearer {os.getenv('TW_OAUTH')}",            
+    }
+
+    url = f'https://api.twitch.tv/helix/streams?user_login={os.getenv("STREAMER")}'
+    try:
+        global online
+        stream = requests.get(url, headers=headers, timeout=5).json()['data'][0]['title']
+        print("3")
+        if online <= 0:
+            for chat_id in read_subs():
+                bot.send_message(chat_id, f'{os.getenv("STREAMER")} запустил стрим!\n{stream}\n\nhttps://www.twitch.tv/{os.getenv("STREAMER")}\nhttps://www.twitch.tv/{os.getenv("STREAMER")}')
+                log(f"SENT TO {chat_id}")
+        online = 3
+        
+    except:
+        print('no stream(')
+        online -= 1
+        print(online)
+
+
+
+def read_subs():
+    with open("subscribers.json", "r") as f:
+        return json.load(f)
+
+
+def add_to_subs(chat_id):
+    chat_id = str(chat_id)
+    subscribers = read_subs()
+    if chat_id not in subscribers:
+        subscribers[chat_id] = True
+        with open("subscribers.json", "w") as f:
+            json.dump(subscribers, f)
+        return(True)
+    else:
+        return(False)
+
+def rem_from_subs(chat_id):
+    chat_id = str(chat_id)
+    subscribers = read_subs()
     if chat_id in subscribers:
         del subscribers[chat_id]
-    update.message.reply_text('Вы отписались от уведомлений. Используйте /start для подписки.')
+        with open("subscribers.json", "w") as f:
+            json.dump(subscribers, f)
+            return True
+    else:
+        return False
 
-# Check stream status every minute and send notification if it goes live
-def check_stream_status(context):
-    response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params={'user_id': streamer_id}).json()
-    if response['data']:
-        for chat_id in subscribers:
-            bot.send_message(chat_id=chat_id, text='{} запустил стрим!'.format('STREAMER_USERNAME'))
+def log(inp):
+    with open('msg.log', 'a') as f:
+        f.write(inp+"\n")
 
-# Set up the dispatcher and add the handlers
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('stop', stop))
+def run_check_stream_status():
+    while True:
+        check_stream_status()
+        time.sleep(30)
 
-# Set up the job queue to check stream status every minute
-job_queue = updater.job_queue
-job_queue.run_repeating(check_stream_status, interval=60, first=0)
-
-# Start the bot
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    # Start a new thread for the check_stream_status() function
+    t = threading.Thread(target=run_check_stream_status)
+    t.start()
+    
+    # Start the bot's polling mechanism in the main thread
+    bot.polling(none_stop=True)
